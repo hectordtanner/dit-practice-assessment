ef6d4f2e43035ea844eecde0ae663f6e5c3a16b5
hectorT <hectordtanner@gmail.com>
2026-03-31 15:01:37 +1300

    added display functionality

--- gather_data.py
+++ gather_data.py
@@ -50,26 +50,28 @@ class GatherDataGUi:
         self.add_data_button.grid(column=1, row=0)
 
         self.name_display_label = Label(self.display_data_frame, text="First name:")
-        self.name_display = Label(self.display_data_frame, textvariable=self.current_name)
+        self.name_display = Label(self.display_data_frame, text="")
         self.name_display_label.grid(column=0, row=1)
         self.name_display.grid(column=1, row=1)
 
         self.age_display_label = Label(self.display_data_frame, text="Age:")
-        self.age_display = Label(self.display_data_frame, textvariable=self.current_age)
+        self.age_display = Label(self.display_data_frame, text="")
         self.age_display_label.grid(column=0, row=2)
         self.age_display.grid(column=1, row=2)
 
-        self.has_phone_label = Label(self.display_data_frame, text="__ has a mobile phone.")
+        self.has_phone_label = Label(self.display_data_frame, text="")
         self.has_phone_label.grid(column=0, row=3, columnspan=2)
 
-        self.previous_button = Button(self.display_data_frame, text="Previous", command=lambda: self.change_display(-1))
-        self.next_button = Button(self.display_data_frame, text="Next", command=lambda: self.change_display(1))
+        self.previous_button = Button(self.display_data_frame, text="Previous", command=lambda: self.change_list_pos(-1))
+        self.next_button = Button(self.display_data_frame, text="Next", command=lambda: self.change_list_pos(1))
         self.previous_button.grid(column=0, row=4)
         self.next_button.grid(column=1, row=4)
     
     def switch_to_display(self):
         self.get_data_frame.pack_forget()
         self.display_data_frame.pack()
+        self.list_pos = 0
+        self.update_display()
 
     def switch_to_add(self):
         self.display_data_frame.pack_forget()
@@ -77,10 +79,26 @@ class GatherDataGUi:
 
     def enter_data(self):
         self.people.append(Person(self.name_entry.get(), self.age_entry.get(), self.current_has_phone.get()))
+        self.age_entry.delete(0, END)
+        self.name_entry.delete(0, END)
+        self.current_has_phone.set(False)
 
-    def change_display(self, amount):
-        print(f"moved {amount} through list")
-
+    def change_list_pos(self, amount):
+        self.list_pos += amount
+        while self.list_pos < 0:
+            self.list_pos += len(self.people)
+        while self.list_pos >= len(self.people):
+            self.list_pos -= len(self.people)
+        self.update_display()
+        
+    def update_display(self):
+        self.name_display.configure(text=self.people[self.list_pos].name)
+        self.age_display.configure(text=self.people[self.list_pos].age)
+        if self.people[self.list_pos].has_phone:
+            self.has_phone_label.configure(text=f"{self.people[self.list_pos].name} has a phone.")
+        else:
+            self.has_phone_label.configure(text=f"{self.people[self.list_pos].name} does not have a phone.")
+    
 if __name__ == "__main__":
     root = Tk()
     gui = GatherDataGUi(root)
