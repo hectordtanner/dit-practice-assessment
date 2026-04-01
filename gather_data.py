f66161f8585421fbd588906b9b428120f1b63a21
hectorT <hectordtanner@gmail.com>
2026-04-01 13:03:59 +1300

    added disable method functionality

--- gather_data.py
+++ gather_data.py
@@ -12,29 +12,30 @@ class GatherDataGUi:
 
     def __init__(self, parent):
         """Creates all GUI elements"""
-        self.current_name = StringVar()
-        self.current_age = IntVar()
         self.current_has_phone = BooleanVar()
         self.current_has_phone.set(False)
         self.people = []
-        self.list_pos = 0
+        self.display_pos = 0
 
         self.get_data_frame = Frame(parent)
         self.display_data_frame = Frame(parent)
         self.get_data_frame.pack()
 
         self.add_data_label = Label(self.get_data_frame, text="Collecting Person Data")
-        self.display_button = Button(self.get_data_frame, text="Show All", command=self.switch_to_display)
+        self.display_button = Button(self.get_data_frame, text="Show All", command=self.switch_to_display, state="disabled")
         self.add_data_label.grid(column=0, row=0)
         self.display_button.grid(column=1, row=0)
 
         self.name_entry_label = Label(self.get_data_frame, text="First name:")
-        self.name_entry = Entry(self.get_data_frame, textvariable=self.current_name)
+        self.name_entry = Entry(self.get_data_frame)
+        self.name_entry.bind("<KeyRelease>", self.disable_buttons)
         self.name_entry_label.grid(column=0, row=1)
         self.name_entry.grid(column=1, row=1)
 
         self.age_entry_label = Label(self.get_data_frame, text="Age:")
-        self.age_entry = Entry(self.get_data_frame, textvariable=self.current_age)
+        self.age_entry = Entry(self.get_data_frame)
+        self.age_entry.insert(0, "0")
+        self.age_entry.bind("<KeyRelease>", self.disable_buttons)
         self.age_entry_label.grid(column=0, row=2)
         self.age_entry.grid(column=1, row=2)
 
@@ -45,7 +46,7 @@ class GatherDataGUi:
         self.has_phone_yes.grid(column=1, row=3)
         self.has_phone_no.grid(column=1, row=4)
 
-        self.enter_data_button = Button(self.get_data_frame, text="Enter Data", command=self.enter_data)
+        self.enter_data_button = Button(self.get_data_frame, text="Enter Data", command=self.enter_data, state="disabled")
         self.enter_data_button.grid(column=0, row=5, columnspan=2)
 
         self.display_label = Label(self.display_data_frame, text="Displaying Person Data")
@@ -66,8 +67,8 @@ class GatherDataGUi:
         self.has_phone_label = Label(self.display_data_frame, text="")
         self.has_phone_label.grid(column=0, row=3, columnspan=2)
 
-        self.previous_button = Button(self.display_data_frame, text="Previous", command=lambda: self.change_list_pos(-1))
-        self.next_button = Button(self.display_data_frame, text="Next", command=lambda: self.change_list_pos(1))
+        self.previous_button = Button(self.display_data_frame, text="Previous", command=lambda: self.change_display_pos(-1))
+        self.next_button = Button(self.display_data_frame, text="Next", command=lambda: self.change_display_pos(1))
         self.previous_button.grid(column=0, row=4)
         self.next_button.grid(column=1, row=4)
     
@@ -76,7 +77,7 @@ class GatherDataGUi:
         """Switches from data frame display frame"""
         self.get_data_frame.pack_forget()
         self.display_data_frame.pack()
-        self.list_pos = 0
+        self.display_pos = 0
         self.update_display()
 
 
@@ -90,31 +91,34 @@ class GatherDataGUi:
         """Adds the input data to the people list (as a Person object)"""
         self.people.append(Person(self.name_entry.get(), self.age_entry.get(), self.current_has_phone.get()))
         self.age_entry.delete(0, END)
+        self.age_entry.insert(0, "0")
         self.name_entry.delete(0, END)
         self.current_has_phone.set(False)
+        self.disable_buttons("")
 
 
-    def change_list_pos(self, amount):
+    def change_display_pos(self, amount):
         """Changes the list position by amount, looping if it excedes the length of the people list"""
-        self.list_pos += amount
-        while self.list_pos < 0:
-            self.list_pos += len(self.people)
-        while self.list_pos >= len(self.people):
-            self.list_pos -= len(self.people)
+        self.display_pos += amount
+        while self.display_pos < 0:
+            self.display_pos += len(self.people)
+        while self.display_pos >= len(self.people):
+            self.display_pos -= len(self.people)
         self.update_display()
 
 
     def update_display(self):
         """Updates the labels on the display frame"""
-        self.name_display.configure(text=self.people[self.list_pos].name)
-        self.age_display.configure(text=self.people[self.list_pos].age)
-        if self.people[self.list_pos].has_phone:
-            self.has_phone_label.configure(text=f"{self.people[self.list_pos].name} has a phone.")
+        self.name_display.configure(text=self.people[self.display_pos].name)
+        self.age_display.configure(text=self.people[self.display_pos].age)
+        if self.people[self.display_pos].has_phone:
+            self.has_phone_label.configure(text=f"{self.people[self.display_pos].name} has a phone.")
         else:
-            self.has_phone_label.configure(text=f"{self.people[self.list_pos].name} does not have a phone.")
+            self.has_phone_label.configure(text=f"{self.people[self.display_pos].name} does not have a phone.")
     
-    def disable_buttons(self):
-        if self.name_entry.get() == "" or self.age_entry.get() <= 0:
+    def disable_buttons(self, arg):
+        """Disables buttons that shouldn't be active"""
+        if self.name_entry.get() == "" or int(self.age_entry.get()) <= 0:
             self.enter_data_button.configure(state="disabled")
         else:
             self.enter_data_button.configure(state="active")
@@ -122,6 +126,7 @@ class GatherDataGUi:
             self.display_button.configure(state="disabled")    
         else:
             self.display_button.configure(state="active") 
+        print(arg)
 
 
 if __name__ == "__main__":
